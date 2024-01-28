3704fbef357be6d2444ce7431030f9e6875e46b8
Mihail Tavyrin <mihailtavyrin2@gmail.com>
2024-01-28 21:08:32 +0300

    New file

--- service/storage.service.js
+++ service/storage.service.js
@@ -1,9 +1,15 @@
-import {homedir} from 'os'
-import {join} from 'path'
+import {homedir} from 'os';
+import {join} from 'path';
+import {promises} from 'fs'
+
 
 const filePath = join(homedir(), 'weather-data.json')
-const saveKey = (key, value)=>{
+const saveKey = async (key, value)=>{
+  const data = {}
 
+
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data))
 }
 
 export {saveKey};
